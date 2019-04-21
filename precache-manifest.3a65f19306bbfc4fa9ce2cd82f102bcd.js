49bebc30585aa70d961a7e7816449a164ceaf19c
Gus <gusrichardson1@gmail.com>
2019-04-21 19:11:19 -0400

    Updates

---
+++ precache-manifest.3a65f19306bbfc4fa9ce2cd82f102bcd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/yelp-events-react-firebase/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bbd98620d56944957787",
+    "url": "/yelp-events-react-firebase/static/css/main.6932b5ef.chunk.css"
+  },
+  {
+    "revision": "d697142b21608a9adca9",
+    "url": "/yelp-events-react-firebase/static/js/runtime~main.817f60d2.js"
+  },
+  {
+    "revision": "64e2bcdc00ff4bf4297948d535138468",
+    "url": "/yelp-events-react-firebase/static/media/toronto.64e2bcdc.jpg"
+  },
+  {
+    "revision": "b396e463c59b6d36eae9",
+    "url": "/yelp-events-react-firebase/static/js/2.c2c947c7.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/yelp-events-react-firebase/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "bbd98620d56944957787",
+    "url": "/yelp-events-react-firebase/static/js/main.be98930d.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/yelp-events-react-firebase/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/yelp-events-react-firebase/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/yelp-events-react-firebase/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "bf60dfbaf0d4925d91b6a721c9f8859b",
+    "url": "/yelp-events-react-firebase/static/media/torontoBlue.bf60dfba.svg"
+  },
+  {
+    "revision": "b396e463c59b6d36eae9",
+    "url": "/yelp-events-react-firebase/static/css/2.933bcf90.chunk.css"
+  },
+  {
+    "revision": "791efb9ccf1115aace3343dae1e09308",
+    "url": "/yelp-events-react-firebase/index.html"
+  }
+];
